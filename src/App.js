0319a794420711c5c13378613788d5b7984c8905
Jhona Rica Manahan <manhan.jrd@gmail.com>
2023-03-08 21:13:56 +0800

    menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { Flex, Heading, IconButton, Spacer, useColorMode, VStack, Link } from '@chakra-ui/react';
+import { Flex, Heading, IconButton, Spacer, useColorMode, VStack, Link, Menu, MenuList, MenuItem, MenuButton, Button } from '@chakra-ui/react';
 import { FaSun, FaMoon, FaGithub, FaLinkedin } from 'react-icons/fa';
 import Home from "./components/Home";
 import Projects from './components/Projects';
 import About from "./components/About";
 import { Link as RouteLink } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
+import { HamburgerIcon } from '@chakra-ui/icons';
+
 
 function App() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -17,6 +19,22 @@ function App() {
         <Link ml={8} as={RouteLink} to={"/Home"}>Home</Link>
         <Link ml={2} as={RouteLink} to={"/projects"}>Projects</Link>
         <Link ml={2} as={RouteLink} to={"/About"}>About</Link>
+      <Menu>
+        <MenuButton as={Button} ml={2}>
+          <HamburgerIcon w={5} h={5} />
+        </MenuButton>
+        <MenuList>
+          <MenuItem><Link as={RouteLink} to={"/Home"}>Home</Link></MenuItem>
+          <MenuItem><Link as={RouteLink} to={"/projects"}>Projects</Link></MenuItem>
+          <MenuItem><Link as={RouteLink} to={"/About"}>About</Link></MenuItem>
+          <Link href='https://www.linkedin.com/in/ijhonamnhn/' target="_blank">
+            <IconButton ml={2} icon={<FaLinkedin />} isRound='true'></IconButton>
+          </Link>
+          <Link href='https://github.com/Jmnahan' target="_blank">
+            <IconButton ml={2} icon={<FaGithub />} isRound='true' onClick={toggleColorMode}></IconButton>
+          </Link>
+        </MenuList>
+      </Menu>
       <Spacer></Spacer>
       <Link href='https://www.linkedin.com/in/ijhonamnhn/' target="_blank">
         <IconButton icon={<FaLinkedin />} isRound='true'></IconButton>
@@ -32,17 +50,6 @@ function App() {
         <Route path="/about" element={<About/>}></Route>
       </Routes>
     </VStack>
-    // <div className="App">
-    //   <header>THIS IS HEADER</header>
-    //   <nav>NAV
-    //     <ul>
-    //       <li>Home</li>
-    //       <li>Projects</li>
-    //       <li>About</li>
-    //     </ul>
-    //   </nav>
-    //   <div>Changing page</div>
-    // </div>
   );
 }
 
